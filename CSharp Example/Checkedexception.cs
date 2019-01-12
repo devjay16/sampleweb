fd9c047fbc812fe6136b14f3dd194aa57d482c1d
Jayveer Mandora <jayveer.mandora@HCL.COM>
2019-01-12 17:26:16 +0000

    sample test

---
+++ CSharp Example/Checkedexception.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Example
+{
+    class Checkedexception
+    {
+        // Use checked overflow context.
+        static void Main()
+        {
+            checked
+            {
+                //Increment up tp max
+                int num = 0;
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    num++;
+                }
+                //Increment up to max again (error)
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    num++;
+                }
+
+            }
+        }
+      }
+}
+
---
+++ CSharp Example/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+                checked
+                {
+                    //Increment up tp max
+                    int num = 0;
+                    for (int i = 0; i < int.MaxValue; i++)
+                    {
+                        num++;
+                    }
+                    //Increment up to max again (error)
+                    for (int i = 0; i < int.MaxValue; i++)
+                    {
+                        num++;
+                    }
+
+                }
+            }
+        }
+    }
+    
+
